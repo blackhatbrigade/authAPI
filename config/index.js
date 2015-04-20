ebab2eb29fee3d24292b1b5a0c969132b11cb281
Jason Bennett <jason.bennett@dominionenterprises.com>
2015-04-20 10:29:59 -0400

    bugfix on ipaddress formate

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = function() {
-  var ipAddress = process.env.OPENSHIFT_NODEJS_IP || 127.0.0.1;
+  var ipAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
   var port = process.env.OPENSHIFT_NODEJS_PORT || 4000;
 
